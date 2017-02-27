42151e654e507912ae3b6f2512bda89dcac89500
Lili Deng (Wicresoft) <v-lide@microsoft.com>
2017-02-27 11:55:26 +0800

    update

--- php_pgsql-freebsd-setup/scripts/install_nginx_php.sh
+++ php_pgsql-freebsd-setup/scripts/install_nginx_php.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
-pkg update
+pkg update -y
 pkg install -y nginx
 pkg install -y php56
 
@@ -18,6 +18,9 @@ sed -i -e 's/^listen = 127.0.0.1:9000/listen = \/var\/run\/php-fpm.sock/g' /usr/
 sed -i -e 's/^;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.conf
 sed -i -e 's/^;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.conf
 sed -i -e 's/^;listen.mode = 0660/listen.mode = 0660/g' /usr/local/etc/php-fpm.conf
+sed -i -e 's/^;events.mechanism = epoll/events.mechanism = kqueue/g' /usr/local/etc/php-fpm.conf
+sed -i -e 's/^pm = dynamic/pm = static/g' /usr/local/etc/php-fpm.conf
+sed -i -e 's/^pm.max_children = 5/pm.max_children = 240/g' /usr/local/etc/php-fpm.conf
 
 echo "<?php" >> /usr/local/www/nginx/index.php
 echo 'echo "Hello world!";' >> /usr/local/www/nginx/index.php
